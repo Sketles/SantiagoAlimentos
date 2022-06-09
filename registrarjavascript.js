6f468a6bcfb564c64382fa160787930ff3fe6624
Dante Velasquez <dan.velasquez@duocuc.cl>
2022-06-09 02:48:31 -0400

    Javascript y nose que wea
    sin comentarios

---
+++ registrarjavascript.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $("#validar").click(function(){
+        var nombre = $("#nombre").val();
+        if(nombre==""){
+            alert("Porfavor rellene el campo nombre");
+        }
+
+        var correo = $("#correo").val();
+        if(correo==""){
+            alert("Porfavor rellene el campo email");
+        }
+
+        var contraseña = $("#contraseña").val();
+        var contraseña2 = $("#contraseña2").val();
+        if(contraseña=="" || contraseña2==""){
+            alert("Porfavor rellene la contraseña")
+        }
+
+    });
+});
